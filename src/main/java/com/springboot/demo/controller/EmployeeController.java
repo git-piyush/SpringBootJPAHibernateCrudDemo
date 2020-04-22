d7732a35acdcfda460e67853c5d6d8fde6757578
Piyush Kumar <piyush-kumar.piyush@dxc.com>
2020-04-22 21:28:11 +0530

    adding retrieve functionality by id

--- src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +42,10 @@ public class EmployeeController {
 			return modelResponse;
 		}
 		
-		
+//		@PostMapping("/")
+//		public EmployeeModelResponse save() {
+//			
+//		}
 		
 		
 		
